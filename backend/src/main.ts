25c642cf898db378610c3f14f2817fdd7d4f3f3f
septem1997 <750627595@qq.com>
2021-05-06 11:05:10 +0800

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'socket.io'
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    https:true,
+    key: fs.readFileSync('./secrets/localhost+2-key.pem'),
+    cert: fs.readFileSync('./secrets/localhost+2.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(3000);
 }
+function initSocketIO(){
+  const io = new Server(3001,{
+    cors:{
+      origin: "*"
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log("用户连接",socket.id)
+    socket.emit("hello", "world");
+  });
+
+
+}
 bootstrap();
+initSocketIO()
