8e14f2ea76f7a8391d182ac454b5f1824720673f
alinlinbo <750627595@qq.com>
2021-05-09 15:06:56 +0800

    new flutter project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,27 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Server } from 'socket.io'
 import * as fs from 'fs';
+import * as https from 'https';
+import * as express from 'express';
+
 
 async function bootstrap() {
   const httpsOptions = {
     https:true,
-    key: fs.readFileSync('./secrets/localhost+2-key.pem'),
-    cert: fs.readFileSync('./secrets/localhost+2.pem'),
+    key: fs.readFileSync('./secrets/localhost+3-key.pem'),
+    cert: fs.readFileSync('./secrets/localhost+3.pem'),
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(3000);
 }
 function initSocketIO(){
-  const io = new Server(3001,{
+  const app = express();
+  const server = https.createServer({
+    key: fs.readFileSync('./secrets/localhost+3-key.pem'),
+    cert: fs.readFileSync('./secrets/localhost+3.pem')
+  },app);
+  server.listen(3001);
+
+  const io = new Server(server,{
     cors:{
-      origin: "*"
+      origin:'*'
     }
   });
 
-  io.on('connection', (socket) => {
-    console.log("用户连接",socket.id)
-    socket.emit("hello", "world");
+  io.on('connection',function (socket) {
+    console.log(socket.id)
   });
+
 
 
 }
