bc4e8ff06c850f6e2cc2180c3de4c714284343d9
chenxiaopei <750627595@qq.com>
2022-05-19 23:46:57 +0800

    feat:edit hall modal

---
+++ admin/components/hall/EditHallModal.vue
@@ -0,0 +1,83 @@
+<template>
+  <n-modal
+      preset="card"
+      @update:show="handleClose"
+      :title="`${id?'编辑':'新增'}大厅`"
+      style="width: 480px"
+      v-model:show="visible">
+    <n-form>
+      <n-form-item label="名称">
+        <n-input v-model:value="hall.name"></n-input>
+      </n-form-item>
+      <n-form-item label="标签">
+        <n-select :options="options" v-model:value="hall.tagId"></n-select>
+      </n-form-item>
+      <n-form-item label="图标">
+        <n-image
+            v-if="hall.roomIcon"
+            :src="hall.roomIcon"
+            style="width: 40px;height: 40px;margin-right: 10px"
+        >
+
+        </n-image>
+        <n-upload
+            :show-file-list="false"
+            :custom-request="customRequest"
+        >
+          <n-button>上传图标</n-button>
+        </n-upload>
+      </n-form-item>
+    </n-form>
+  </n-modal>
+</template>
+
+<script lang="ts" setup>
+import {defineEmits} from "@vue/runtime-core";
+import {reactive, ref, watch} from "#imports";
+import {$fetch} from "ohmyfetch";
+import {UploadCustomRequestOptions} from "naive-ui";
+
+const emit = defineEmits(['update:visible'])
+const props = defineProps({
+  visible: Boolean,
+  id: {
+    type: Number
+  }
+})
+const hall = reactive({})
+const options = ref([])
+const customRequest = async ({file}: UploadCustomRequestOptions) => {
+  const formData = new FormData()
+  formData.append('file', file.file as File)
+  const res = await $fetch("/api/upload", {
+    method: 'post',
+    body: formData
+  })
+  hall.roomIcon = res.data
+}
+watch(() => props.visible, async (visible) => {
+  if (visible) {
+    let hallInfo = {
+      name: '',
+      tagId: null,
+      roomIcon: null,
+    }
+    const res = await $fetch('/api/room/tags')
+    options.value = res.data.map(item =>
+        ({value: item.id, label: item.name}))
+    if (props.id) {
+      const res = await $fetch('/api/room/detail?id=' + props.id)
+      hallInfo = res.data
+      hallInfo.tagId = res.data.tag.id
+    }
+    Object.assign(hall, hallInfo)
+  }
+})
+const handleClose = (visible) => {
+  emit('update:visible', visible)
+}
+</script>
+
+<style scoped>
+
+</style>
--- admin/pages/hall/index.vue
+++ admin/pages/hall/index.vue
@@ -1,22 +1,32 @@
 <template>
-  <n-data-table
-      :columns="columns"
-      :data="data"
-      :pagination="pagination"
-      :bordered="false"
-  />
+  <div>
+    <n-form-item>
+      <n-button @click="addHall">新增大厅</n-button>
+    </n-form-item>
+    <n-data-table
+        :columns="columns"
+        :data="data"
+        :pagination="pagination"
+        :bordered="false"
+    />
+    <edit-hall-modal :id="editId" v-model:visible="editVisible" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import {definePageMeta, onMounted, reactive, ref} from "#imports";
-import {DataTableColumns, NAvatar, NImage, PaginationInfo, PaginationProps} from "naive-ui";
+import {DataTableColumns, NAvatar, NButton, NImage, PaginationInfo, PaginationProps} from "naive-ui";
 import {$fetch} from "ohmyfetch";
+import EditHallModal from "~/components/hall/EditHallModal.vue";
+import {Ref} from "@vue/reactivity";
 
 definePageMeta({
   name: "index",
   layout: "admin"
 })
 const data = ref([] as Room[])
+const editVisible = ref(false)
+const editId:Ref<null|number> = ref(null)
 const pagination = reactive({
   page: 1,
   pageSize: 10,
@@ -32,10 +42,14 @@ const getList = async (page = 1, pageSize = 10) => {
   pagination.page = page
   pagination.itemCount = res.data.total
 }
-const handlePageChange = (page)=>{
+const handlePageChange = (page) => {
   getList(page)
 }
-onMounted(()=>getList())
+const addHall = ()=>{
+  editId.value = null
+  editVisible.value = true
+}
+onMounted(() => getList())
 const columns: DataTableColumns = [{
   title: "编号",
   key: "roomNo"
@@ -45,15 +59,15 @@ const columns: DataTableColumns = [{
 }, {
   title: "图标",
   key: "roomIcon",
-  render(row){
+  render(row) {
     return h(
         NImage,
         {
-          style:{
-            width:'40px',
-            height:'40px'
+          style: {
+            width: '40px',
+            height: '40px'
           },
-          src:row.roomIcon
+          src: row.roomIcon
         }
     )
   }
@@ -63,6 +77,20 @@ const columns: DataTableColumns = [{
 }, {
   title: "创建时间",
   key: "createTime"
+},{
+  title: "操作",
+  key: "action",
+  render(row){
+    return h(NButton,{
+      text:true,
+      tag:'a',
+      type:"primary",
+      onClick:()=>{
+        editId.value = row.id as number
+        editVisible.value = true
+      }
+    },'编辑')
+  }
 }]
 </script>
 
